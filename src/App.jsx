c013ca8e6c0de62cc42d73a393f6157f82b1214c
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-10-14 05:41:45 +0200

    Licht functie toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [licht, setLicht] = React.useState(false);
     return (
-        <main className="off">
+        <main className={licht === true ? "on" : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button" onClick={() => setLicht(!licht)}>
+                {licht === false ? 'Turn on' : 'Turn off'}</button>
             </section>
         </main>
     );
